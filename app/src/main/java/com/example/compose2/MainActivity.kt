e8201343ef0a1259269eae2343d3b498ed8213ce
arysugiarto <arysugiarto10@gmail.com>
2022-11-06 08:16:24 +0700

    LazyColumn

--- app/src/main/java/com/example/compose2/MainActivity.kt
+++ app/src/main/java/com/example/compose2/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.compose2
 
+import android.nfc.cardemulation.CardEmulation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
@@ -20,6 +30,9 @@ import androidx.compose.ui.unit.sp
 import com.example.compose2.ui.theme.Compose2Theme
 import org.w3c.dom.NameList
 
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+
 private val sampleName = listOf(
     "Andre",
     "Desta",
@@ -28,7 +41,9 @@ private val sampleName = listOf(
     "Komeng",
     "Raffi Ahmad",
     "Andhika Pratama",
-    "Vincent Ryan Rompies"
+    "Vincent Ryan Rompies",
+    "David",
+    "Jaka"
 )
 
 
@@ -45,27 +60,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
     Row(
         Modifier.padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(painter = painterResource(id = R.drawable.jetpack_compose),
             contentDescription = "Logo Jetpack Compose",
-        modifier = Modifier.size(80.dp)
+        modifier = Modifier.size(animatedSizeDp)
+
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+        Column(modifier = Modifier.weight(1f)) {
             Text(text = "Hello $name!",
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
             )
             Text(text = "Hello ary")
         }
+        IconButton(onClick = {isExpanded = !isExpanded}) {
+            Icon(
+                imageVector= if(isExpanded)Icons.Filled.ExpandLess else Icons.Outlined.ExpandMore,
+                contentDescription = "Show More"
+            )
+
+        }
 
     }
 
 }
-
 
 @Composable
 fun HelloJetpactkComposeApp() {
@@ -75,7 +106,6 @@ fun HelloJetpactkComposeApp() {
         ) {
             GreetingList(sampleName)
         }
-
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
@@ -88,11 +118,17 @@ fun HelloJetpackComposePreview(){
 @Composable
 fun GreetingList(names: List<String>){
     if (names.isNotEmpty()){
-        Column {
-            for (name in names){
-                Greeting(name = name)
+//        Column {
+//            for (name in names){
+//                Greeting(name = name)
+//            }
+//        }
+        LazyColumn{
+            items(names){ name->
+                Greeting(name)
             }
         }
+        
     }else{
         Text("No People to great :(" )
     }
